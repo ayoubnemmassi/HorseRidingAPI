aefb23d8befbe0a1134158c696b2db39e622c00c
ayoubnemmassi <ayoub.nemmassi@gmail.com>
2021-05-30 17:35:34 +0100

    more functions

---
+++ Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace HorseRidingAPI.Models
+{
+    public partial class Note
+    {
+        public int Id { get; set; }
+        public string Notes { get; set; }
+        public DateTime? Date { get; set; }
+        public int? ClientId { get; set; }
+
+        public virtual Client Client { get; set; }
+    }
+}
