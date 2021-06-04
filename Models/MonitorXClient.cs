45e1a710e3c92b1c72d87454168dcd9fc6f0d278
ayoubnemmassi <ayoub.nemmassi@gmail.com>
2021-06-04 23:57:11 +0100

    version 1

---
+++ Models/MonitorXClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HorseRidingAPI.Models
+{
+    public class MonitorXClient
+    {
+        public int SeanceId { get; set; }
+        public int SeanceGrpId { get; set; }
+        public string ClientId { get; set; }
+        public string MonitorId { get; set; }
+        public DateTime StartDate { get; set; }
+        public byte DurationMinut { get; set; }
+        public byte IsDone { get; set; }
+        public int? PaymentId { get; set; }
+        public string Comments { get; set; }
+
+       
+    }
+}
